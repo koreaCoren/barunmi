78c93505f663f752b39add7fec120a3d70a76b6c
jung woo Kwon <kwog5050@naver.com>
2023-02-21 10:35:07 +0900

    이미지작업끝

--- src/pages/subPage/contact/Contact.js
+++ src/pages/subPage/contact/Contact.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { Route, Switch, useParams } from "react-router-dom";
-  
+
 function Contact() {
   let { id } = useParams();
-  
+
   return (
     <>
-      <div className="wrap">
-        <img style={{ width:'100%'}} src={`/img/subPage/contact/contact0${id}_content.jpg`} alt="" className="pcimg"/>
-        <img style={{ width:'100%'}} src={`/img/subPage/contact/mobile/contact0${id}_content.jpg`} alt="" className="moimg"/>
-      </div>
+      <img style={{ width: '100%' }} src={`/img/subPage/contact/contact0${id}_content.jpg`} alt="" className="pcimg" />
+      <img style={{ width: '100%' }} src={`/img/subPage/contact/800/contact0${id}_content.jpg`} alt="" className="submoimg800" />
     </>
   )
 }
