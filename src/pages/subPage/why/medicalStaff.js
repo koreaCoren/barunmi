35825cc2652f43ab4a1fdf8edeb33a920a9ba54b
pelshad <akuaru@gmail.com>
2023-02-21 10:38:40 +0900

    의료진

---
+++ src/pages/subPage/why/medicalStaff.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function medicalStaff() {
+  return (
+    <>
+      <div>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+}
